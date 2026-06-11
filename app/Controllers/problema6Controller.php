405d5e41267d52934cf95a014f80cb5460b8fd46
irvin1305 <irvingonzalez00213@gmail.com>
2026-06-10 19:24:36 -0500

    ya lo arregle charlotte

--- app/Controllers/problema6Controller.php
+++ app/Controllers/problema6Controller.php
@@ -1,14 +1,18 @@
 <?php
-
 require_once 'app/Models/PresupuestoHospital.php';
 
 class Problema6Controller
 {
     public function procesar()
     {
-        $presupuestoTotal = 20000; // puedes cambiarlo o tomarlo de un formulario
-        $hospital = new PresupuestoHospital($presupuestoTotal);
-        $resultado = $hospital->calcular();
+        $resultado = null;
+
+        // Revisar si se envió el formulario
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['presupuesto'])) {
+            $presupuestoTotal = floatval($_POST['presupuesto']);
+            $hospital = new PresupuestoHospital($presupuestoTotal);
+            $resultado = $hospital->calcular();
+        }
 
         require_once 'app/Views/problemas/problema6.php';
     }
